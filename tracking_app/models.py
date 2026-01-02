2ea25dbb9db5a7676961d61cd7e55a1a96401649
Adam Halka <galkaadam4@gmail.com>
2026-01-02 11:06:16 +0100

    Add models in tracking_app/models.py

--- tracking_app/models.py
+++ tracking_app/models.py
@@ -1,3 +1,172 @@
 from django.db import models
+from django.db.models import Q, F
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    closest_big_city = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="route_from"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="route_to"
+    )
+    distance = models.PositiveIntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["source", "destination"],
+                name="unique_route"
+            ),
+            models.CheckConstraint(
+                check=~Q(source=F("destination")),
+                name="source_not_equal_destination"
+            )
+        ]
+
+    def __str__(self):
+        return f"Flight from {self.source.name} to {self.destination.name}"
+
+    class Meta:
+        unique_together = (("source", "destination"),)
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.PositiveIntegerField()
+    seats_in_row = models.PositiveIntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType,
+        on_delete=models.CASCADE,
+        related_name="airplanes"
+    )
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+
+    def __str__(self):
+        return f"Airplane: {self.name} with {self.capacity} seats"
+
+
+class Flight(models.Model):
+    route = models.ForeignKey(
+        Route,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        on_delete=models.CASCADE,
+        related_name="flights"
+    )
+    crew = models.ManyToManyField(Crew, related_name="flights")
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(arrival_time__gt=F("departure_time")),
+                name="arrival_after_departure"
+            )
+        ]
+
+    def __str__(self):
+        return (f"Airplane {self.airplane.name} flies at "
+                f"{self.departure_time} to "
+                f"{self.route.destination.name}")
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = User
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    row = models.PositiveIntegerField()
+    seat = models.PositiveIntegerField()
+    flight = models.ForeignKey(
+        Flight,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name="tickets"
+    )
+
+    @staticmethod
+    def validate_ticket(row, seat, airplane, error_to_raise):
+        for ticket_attr_value,ticket_attr_name, airplane_attr_name in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats_in_row"),
+        ]:
+            count_attrs = getattr(airplane, airplane_attr_name)
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: f"{ticket_attr_value} "
+                        f"number must be in available range "
+                        f"(1, {airplane_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.flight.airplane,
+            ValidationError
+        )
+
+    def save(
+            self,
+            *args,
+            force_insert=False,
+            force_update=False,
+            using=None,
+            update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert=force_insert,
+            force_update=force_update,
+            using=using,
+            update_fields=update_fields,
+        )
+
+    def __str__(self):
+        return (
+            f"{str(self.flight)} row: {self.row}, seat: {self.seat}"
+        )
