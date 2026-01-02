f2285d3c7cfeab68432a57ea210dbcb78a3068e4
Adam Halka <galkaadam4@gmail.com>
2026-01-02 12:37:18 +0100

    add basic views in tracking_app/views.py

--- tracking_app/views.py
+++ tracking_app/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from tracking_app.models import (
+    Airport,
+    Route,
+    Crew,
+    Airplane,
+    AirplaneType, Flight, Order
+)
+from tracking_app.permissions import IsAdminOrIfAuthenticatedReadOnly
+from tracking_app.serializers import (
+    AirportSerializer,
+    RouteSerializer,
+    CrewSerializer,
+    AirplaneSerializer,
+    AirplaneTypeSerializer, FlightSerializer, OrderSerializer
+)
+
+
+class AirportViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class AirplaneViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class FlightViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
+
+
+class OrderViewSet(
+mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
